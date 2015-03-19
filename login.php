42c12208009678bbd6ee0da264599eaabd09124f
cheissenberger <chris@cheissenberger.com>
2015-03-19 08:44:08 +0100

    Finished Login
    Added check if User is teacher

--- login.php
+++ login.php
@@ -1,21 +1,30 @@
 <?php
 	session_start();
-	$ldaprdn  = htmlspecialchars($_POST["name"]).'@htlw3r.ac.at';//cn=user, cn=Users, dc=HTLW3R, dc=ac, dc=at';
-	$ldappass = htmlspecialchars($_POST["pw"]);  // entsprechendes password
-
-// verbinden zum ldap server
-	$ldapconn = ldap_connect("ldap://10.0.0.210/") or die("Keine Verbindung zum LDAP server möglich.");
+	$ldaprdn  = htmlspecialchars($_POST["name"]).'@htlw3r.ac.at';
+	$ldappass = htmlspecialchars($_POST["pw"]);
+	$ldapconn = ldap_connect("ldap://10.0.0.210/") or die("Keine Verbindung zum Server möglich.");
 	    
-	// Todo catch errors and handle them appropriately
+	// Todo: catch errors and handle them appropriately
+	
 	if ($ldapconn) {
-	
-	    // binden zum ldap server
 	    $ldapbind = ldap_bind($ldapconn, $ldaprdn, $ldappass);
-	
-	    // Bindung überpfrüfen
+
 	    if ($ldapbind) {
-		    $_SESSION['user'] = htmlspecialchars($_POST["name"]);
-		    ldap_close($ldapconn);
+		    $result = ldap_search($ldapconn, "ou=Lehrer,dc=HTLW3R,dc=ac,dc=at", "(|(cn=".htmlspecialchars($_POST["name"])."))");
+			    
+		    foreach(ldap_get_entries($ldapconn, $result) as $val) {
+			    if ($val == 0) {
+				    session_unset();
+					session_destroy();
+				    die("Keine ausreichenden Rechte");
+			    }
+			    else if ($val == 1) {
+				    $_SESSION['user'] = htmlspecialchars($_POST["name"]);
+				    break;
+			    }
+		    }
+
+		    ldap_close($ldapconn);		    
 			header("Location: dash.php");
 			
 	    } else {

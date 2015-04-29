a1af8a770f24c4297c782e8e1e95fc9d717fd117
xxtompsonxx <tom97@aon.at>
2015-04-29 10:15:59 +0200

    Favoriten removenk

--- startbootstrap-sb-admin-1.0.2/profil.php
+++ startbootstrap-sb-admin-1.0.2/profil.php
@@ -156,6 +156,11 @@
 
     <!-- jQuery -->
     <script src="js/jquery.js"></script>
+    <script>
+    $(document).ready(function(){
+        $('.icon_favoriten').click(function(){
+    });
+    </script>
 
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>

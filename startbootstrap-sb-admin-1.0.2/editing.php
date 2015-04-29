9b73f6bdcde9a27ae551aabbb2706b699d043c6e
Raffael Tunder <raffael.tunder@gmail.com>
2015-04-29 10:41:02 +0200

    für das bearbeiten eine phpph seit lol

---
+++ startbootstrap-sb-admin-1.0.2/editing.php
@@ -0,0 +1,205 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>HTL3R-Curriculum</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/sb-admin.css" rel="stylesheet">
+
+    <!-- Morris Charts CSS -->
+    <link href="css/plugins/morris.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <style>
+        .top-nav>li>a {
+            color:white;   
+        }
+
+        .chooseweek{
+            width: 100%;
+            text-align: center;
+            margin: 0 auto;
+        }
+
+        .chooseweek>a{
+            margin: 30px;    
+        }
+
+        .chooseweek>a:hover{
+            background-color: #07f9f9;
+        }
+    </style>
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation" >
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.html" style="color: white;">HTL3R-Curriculum</a>
+            </div>
+            <!-- Top Menu Items -->
+            <ul class="nav navbar-right top-nav">
+              
+
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> Andy Bichl <b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a href="#"><i class="fa fa-fw fa-user"></i> Profil</a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-fw fa-gear"></i> Einstellungen</a>
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                            <a href="#"><i class="fa fa-fw fa-power-off"></i>  Ausloggen</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+            <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav side-nav">
+                    <li>
+                        <a href="index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+                    </li>
+                    <li>
+                        <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="glyphicon glyphicon-education"></i> Ausbildungszweige <i class="fa fa-fw fa-caret-down"></i></a>
+                        <ul id="demo" class="collapse">
+                            <li class="active">
+                                <a href="it.php">Informationstechnologie</a>
+                            </li>
+                            <li>
+                                <a href="#">Mechatronik</a>
+                            </li> 
+                            <li>
+                                <a href="#">Fachschule</a>
+                            </li>
+                        </ul>
+                    </li>
+                    
+                    <li>
+                        <a href="lehrplaene.php"><i class="glyphicon glyphicon-book"></i> Lehrpläne</a>
+                    </li>
+                    
+                    <li>
+                        <a href="profil.php"><i class="fa fa-fw fa-user"></i>Profil</a>
+                    </li>
+                    
+                    <li>
+                        <a href="faq.php"><i class="glyphicon glyphicon-question-sign"></i> FAQ</a>
+                    </li>
+                    <li>
+                        <a href="impressum.php"><i class="glyphicon glyphicon-list-alt"></i> Impressum</a>
+                    </li>
+      
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </nav>
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+            <div id="lehrplaene_edit_lol">
+                <form name="edit_frm" action="editing.php" method="get">
+                    <select name="subject_sel">
+                        <option>
+                            Mathe
+                        </option>
+                    </select>
+                    <select name="jahrgang_sel">
+                        <option>
+                            Jahrgang 1
+                        </option>
+                        <option>
+                            Jahrgang 2
+                        </option>
+                    </select>
+                    <input type="submit" value="display" />
+                </form>
+                <table class="table">
+                <thead>
+                  <tr>
+                    <th>Woche</th>
+                    <th>Description</th>
+                    <th>Kalenderwoche</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>1</td>
+                    <td>Sinuskurven for dumb people</td>
+                    <td>90</td>
+                  </tr>
+                  <tr>
+                    <td>2</td>
+                    <td>Sinuskurven for dumb people</td>
+                    <td>90</td>
+                  </tr>
+                  <tr>
+                    <td>3</td>
+                    <td>Sinuskurven for dumb people</td>
+                    <td>90</td>
+                  </tr>
+                </tbody>
+              </table>
+              <div class="chooseweek">
+              <a href="#">1-10</a><a href="#">11-20</a><a href="#">21-30</a><a href="#">31-40</a>
+              </div>
+            </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Morris Charts JavaScript -->
+    <script src="js/plugins/morris/raphael.min.js"></script>
+    <script src="js/plugins/morris/morris.min.js"></script>
+    <script src="js/plugins/morris/morris-data.js"></script>
+
+</body>
+
+</html>

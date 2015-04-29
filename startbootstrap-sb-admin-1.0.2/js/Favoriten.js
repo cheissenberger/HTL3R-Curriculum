9dd2b4b19b65e9183653e50ba7e7299179b563b5
Michael Langstöger <Taiboss@gmx.at>
2015-04-29 08:13:13 +0200

    Favoriten

--- startbootstrap-sb-admin-1.0.2/js/Favoriten.js
+++ startbootstrap-sb-admin-1.0.2/js/Favoriten.js
@@ -1,13 +1,23 @@
+$(document).ready(function{
+
+alert("Hurra");
+//Adds favorite to the profile
+function add_favorite(input){
+	
+	
+	
+};
+
+//Removes favorite from the profile
+function remove_favorite(input){
+	
+	
+	
+};
+
+$(.fa-heart).click(add_favorite);
+
+$(.fa-heart).click(remove_favorite($(this));
 
 
-function add_favorite(){
-	
-	
-	
-}
-
-function remove_favorite(){
-	
-	
-	
-}
+});
